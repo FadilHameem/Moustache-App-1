a91f88aeeffa4889371ac482c632e6d094d678db
FadilHameem <105495109+FadilHameem@users.noreply.github.com>
2022-12-14 06:36:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(1000,1000);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('Moustache_Image.jpg');
+}
